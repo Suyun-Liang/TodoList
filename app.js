3f53ef1d7d1795eae541ec50fb873f5b85cd96b4
LiangSuyun <lsyxxl123@gmail.com>
2023-07-18 16:46:02 +0100

    add mongodb and remove date.js local module

--- app.js
+++ app.js
@@ -1,70 +1,117 @@
 const express = require('express');
-// require local module which binds all the exports to the const date 
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const { name } = require('ejs');
 
 const app = express();
 const port = 3000;
 
-// setup templates
+// setup templates, bodyparser, local files
 app.set('view engine', 'ejs');
-// setup bodyparser
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// serve up all the files in public folder eg .css .png
 app.use(express.static("public"));
 
-// store user input data
-const items = ["buy sth", "cook sth", "eat sth"];
-const workItems = ["study", "javascript fundation"];
+// create a database
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
 
-
-/* page ListMain */
-
-
-app.get('/', function(req, res) {
-    
-    // call the function thats bound to const date, get the result and assign it to the todayStr
-    const todayStr = date.getDate();
-
-    // send user templating file with dynamic date, items array, and the path of the page
-    res.render('ListMain', {listTitle: todayStr, listItems: items, path: req.url});
+const itemsSchema = mongoose.Schema({
+    name: String
 });
+const listSchema = mongoose.Schema({
+    name: String,
+    items: [itemsSchema]
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+const List = mongoose.model('List', listSchema);
+
+const item1 = new Item({
+    name: 'Welcome to your todolist!'
+});
+const item2 = new Item({
+    name: 'Hit the + button to add a new item.'
+});
+const item3 = new Item({
+    name: '<-- Hit this to delete an item.'
+});
+const defaultItems = [item1, item2, item3];
+
+/* Main page*/
+
+app.get('/', function(req, res) { 
+
+    Item.find({})
+    .then(value => {
+        if(value.length === 0) {
+            Item.create(defaultItems)
+                .then(() => console.log('successfully insert default items'))
+                .catch(err => console.log(err));
+            res.redirect('/');
+        }
+
+        res.render('ListMain', {listTitle: "Today", listItems: value, path: req.path});
+    })
+    .catch(err => console.log(err));
+    
+});
+
 
 app.post('/', function(req, res) {
-    
-    // collect user input
-    const item = req.body.newItem;
-    
-    // store each input into an array called items
-    items.push(item);
+        
+    const input = req.body.newItem;
+    const listName = req.body.list;
 
-    res.redirect('/');
+    const item = new Item({
+        name: input
+    });
+
+    if(listName === 'Today') {
+        Item.create(item);
+        res.redirect('/');
+    }else {
+        List.findOneAndUpdate({name: listName}, {$push:{items: item}})
+            .then(() => res.redirect('/' + listName))
+            .catch(err => console.log(err)); 
+    }
+  
 });
 
+/* Delete an item from list */
 
-/* Page Work */
+app.post('/delete', function(req, res) {
+    const checkedId = req.body.checkbox;
+    const listName = req.body.listName;
 
+    if(listName === 'Today') {
+        Item.findByIdAndRemove(checkedId)
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err));  
+    }else {
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id : checkedId}}})
+            .then(() => res.redirect('/' + listName))
+            .catch(err => console.log(err));
+    }
 
-app.get('/work', function(req, res){
     
-    res.render('ListMain', {listTitle: 'Work List', listItems: workItems, path: req.url});
 });
 
+/* CustomerList page */
 
-app.post('/work', function(req, res) {
+app.get('/:customListName', function(req,res) {
+    const customListName = _.capitalize(req.params.customListName);
     
-    const item = req.body.newItem;
-
-    workItems.push(item);
-
-    res.redirect('/work');   
-});
-
-
-/* Page About */
-
-app.get('/about', function(req, res) {
-    res.render("about");
+    List.findOne({name: customListName})
+        .then((list)=> {
+            if(!list) {
+                List.create({name: customListName, items: defaultItems})
+                .then(() => res.redirect('/' + customListName))
+                .catch(err => console.log(err));
+            }else{
+                res.render('ListMain', {listTitle: customListName, listItems: list.items, path: '/'});
+            }
+        })
+        .catch(err => console.log(err));
 });
 
 
